646d63e05671acc80adf58e8ff42b1107b12491a
mobeets <mobeets@gmail.com>
2014-05-30 10:24:55 -0500

    slide run for all subjs; fixed up pd plotting args

--- bin/pd_pcorVsCohByDur.py
+++ bin/pd_pcorVsCohByDur.py
@@ -1,89 +1,11 @@
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import linregress
-from scipy.optimize import minimize
 
 from tools import color_list
-from pd_io import load, default_filter_df
+from pd_io import load
 
-def params(theta):
-    if len(theta) == 2:
-        a, b = theta
-        maxV = 1.0
-    elif len(theta) == 3:
-        a, b, maxV = theta
-    else:
-        raise Exception("params must be length 2 or 3: {0}".format(theta))
-    minV = 0.5
-    return a, b, minV, maxV
-
-def weibull(x, theta):
-    """
-    theta = scale, shape, maxV [optional, in case performance is less than 1]
-    """
-    a, b, minV, maxV = params(theta)
-    return maxV - (maxV-minV) * np.exp(-pow(x/a, b))
-
-def inv_weibull(theta, y):
-    """
-    the function calculates the inverse of a weibull function
-    with given parameters (theta) for a given y value
-    returns the x value
-    """
-    a, b, minV, maxV = params(theta)
-    return a * pow(np.log((maxV-minV)/(maxV-y)), 1.0/b)
-
-def weibull_mle(theta, xs, ys):
-    yh = weibull(xs, theta)
-    logL = np.sum(ys*np.log(yh) + (1-ys)*np.log(1-yh))
-    if np.isnan(logL):
-        yh = yh*0.99 + 0.005
-        logL = np.sum(ys*np.log(yh) + (1-ys)*np.log(1-yh))
-    return -logL
-
-def solve(xs, ys, guess=(0.3, 1.0), ntries=10):
-    neg_log_likelihood_fcn = lambda theta: weibull_mle(theta, xs, ys)
-    for i in xrange(ntries):
-        if i > 0:
-            guess = (guess[0] + i/10.0, guess[1] + i/10.0)
-        soln = minimize(neg_log_likelihood_fcn, guess, method='TNC', bounds=[(0, None), (0, None)], constraints=[])
-        if soln['success']:
-            theta_hat = soln['x']
-            return theta_hat
-    return None
-
-def solve_all_durations(df, coh_thresh=0.75):
-    threshes, thetas = {}, {}
-    for di in df['duration_index'].unique():
-        dfc = df[df['duration_index'] == di]
-        xs = dfc['coherence'].values
-        ys = dfc['correct'].values.astype('float')
-        thetas[di] = solve(xs, ys)
-        threshes[di] = inv_weibull(thetas[di], coh_thresh) if thetas[di] is not None else None
-        if thetas[di] is None:
-            print 'ERROR   (di={0})'.format(di)
-        else:
-            print 'SUCCESS (di={0}): {1}'.format(di, thetas[di])
-    return threshes, thetas
-
-def main_fit(args):
-    df = default_filter_df(load(args))
-    durmap = dict(df.groupby('duration_index')['duration'].agg(min).reset_index().values)
-    for dotmode, df_dotmode in df.groupby('dotmode'):
-        threshes, thetas = solve_all_durations(df_dotmode)
-        xs, ys = zip(*[(durmap[di], threshes[di]) for di in sorted(threshes) if threshes[di] is not None])
-        slope, intercept, r_value, p_value, std_err = linregress(np.log(xs), np.log(ys))
-        print '{4}: slope={0}, intercept={1}, r^2={2}, s.e.={3}'.format(slope, intercept, r_value**2, std_err, dotmode)
-        plt.plot(np.log(xs), slope*np.log(xs) + intercept, color='g' if dotmode == '2d' else 'r')
-        plt.scatter(np.log(xs), np.log(ys), label=dotmode, color='g' if dotmode == '2d' else 'r')
-    plt.title('75% coh thresh vs. duration')
-    plt.xlabel('log(duration)')
-    plt.ylabel('log(75% coh thresh)')
-    plt.legend()
-    plt.show()
-
-def plot_raw(df):
+def plot(df):
     durinds = df['duration_index'].unique()
     durmap = dict(df.groupby('duration_index')['duration'].agg(min).reset_index().values)
     cols = color_list(len(durinds))
@@ -105,15 +27,14 @@ def plot_raw(df):
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     plt.show()
 
-def main_raw(args):
+def main(args):
     df = load(args)
     for dotmode, df_dotmode in df.groupby('dotmode'):
-        plot_raw(df_dotmode)
+        plot(df_dotmode)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--subj", required=False, type=str, help="")
     parser.add_argument("--dotmode", required=False, type=str, help="")
     args = parser.parse_args()
-    # main_raw(vars(args))
-    main_fit(vars(args))
+    main(vars(args))
--- bin/pd_plot.py
+++ bin/pd_plot.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 from matplotlib.mlab import griddata
 from mpl_toolkits.mplot3d import Axes3D
 
-def make_surface(xs, ys, zs, resX=100, resY=100):
+def make_surface(xs, ys, zs, resX=50, resY=50):
     xi = np.linspace(min(xs), max(xs), resX)
     yi = np.linspace(min(ys), max(ys), resY)
     Z = griddata(xs, ys, zs, xi, yi, interp='linear')
     X, Y = np.meshgrid(xi, yi)
     return X, Y, Z
 
-def plot_inner(data, fig, color):
+def plot_inner(data, fig, color, durmap):
     if len(data) == 2:
         ax = fig.gca(projection='2d')
         ax.scatter(*data, color=color)
@@ -18,10 +18,11 @@ def plot_inner(data, fig, color):
         ax = fig.gca(projection='3d')
         x,y,z = data
         x = np.log(100.*np.array(x))
+        y = np.log([1000.*durmap[i] for i in y])
         ax.scatter(x,y,z, color=color)
         ax.plot_wireframe(*make_surface(x,y,z), color=color)
         ax.set_xlabel('log(coherence)')
-        ax.set_ylabel('duration bin')
+        ax.set_ylabel('log(duration (ms))')
         ax.set_zlabel('p correct')
         ax.set_xlim([0.5, 5.0])
         ax.set_zlim([0.3, 1.0])
@@ -41,8 +42,9 @@ def plot(df, show=True, outfile=None, fig=None):
         return
     if fig is None:
         fig = plt.figure()
+    durmap = dict(df.groupby('duration_index')['duration'].agg(min).reset_index().values)
     for dotmode, df_dotmode in df.groupby('dotmode'):
         df2 = df_dotmode[['coherence', 'duration_index', 'correct']]
         df3 = df2.groupby(['coherence', 'duration_index'], as_index=False).aggregate(np.mean)
-        plot_inner(zip(*df3.values), fig, 'g' if dotmode == '2d' else 'r')
+        plot_inner(zip(*df3.values), fig, 'g' if dotmode == '2d' else 'r', durmap)
     save_or_show(show, outfile)
