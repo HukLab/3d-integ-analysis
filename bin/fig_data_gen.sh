b528ac6df9b91dffe32d3d954a004d5a7f381e86
mobeets <mobeets@gmail.com>
2014-08-04 11:41:26 -0500

    cleaning pcorVsDurByCoh

--- bin/fig_data_gen.sh
+++ bin/fig_data_gen.sh
@@ -14,22 +14,22 @@ mkdir -p $OUTDIR
 # -----------------
 # by dotmode, SUBJ
 echo "Psychometric function for $MODEL_SUBJ..."
-python pcorVsCohByDur.py --subj $MODEL_SUBJ --join-dotmode --thresh --nboots $NBOOTS_THRESH --outdir $OUTDIR --savefig
+python pmf_fit.py --subj $MODEL_SUBJ --ignore-dur --nboots $NBOOTS_THRESH --outdir $OUTDIR
 # by dotmode, ALL
 echo "Psychometric function for ALL..."
-python pcorVsCohByDur.py --join-dotmode --thresh --nboots $NBOOTS_THRESH --outdir $OUTDIR --savefig
+python pmf_fit.py --ignore-dur --nboots $NBOOTS_THRESH --outdir $OUTDIR
 # by duration, ALL, (2D, 3D)
 echo "Psychometric function for ALL (2D, 3D)..."
-python pcorVsCohByDur.py --thresh --plot-thresh --nboots $NBOOTS_THRESH --outdir $OUTDIR --savefig
+python pmf_fit.py --nboots $NBOOTS_THRESH --outdir $OUTDIR
 
 # SAT_EXP -- pcor vs dur
 # --------
 # by dotmode, SUBJ
 echo "SAT_EXP for $MODEL_SUBJ..."
-python pcorVsDurByCoh.py --subj $MODEL_SUBJ --join-dotmode --outdir $OUTDIR --savefig
+python pcorVsDurByCoh.py --subj $MODEL_SUBJ --fit --outdir $OUTDIR
 # by dotmode, ALL
 echo "SAT_EXP for ALL..."
-python pcorVsDurByCoh.py --join-dotmode --outdir $OUTDIR --savefig
+python pcorVsDurByCoh.py --fit --outdir $OUTDIR
 # by coherence, ALL, (2D, 3D)
 echo "SAT_EXP for ALL (2D, 3D)..."
 python fitCurveVsDurByCoh.py --is_agg_subj --fits sat-exp --nboots $NBOOTS_SAT_EXP --outdir $OUTDIR
--- bin/pcorVsDurByCoh.py
+++ bin/pcorVsDurByCoh.py
@@ -10,6 +10,49 @@ from tools import color_list
 import saturating_exponential
 from settings import DEFAULT_THETA, min_dur, max_dur
 
+durmap_fcn = lambda df: dict(df.groupby('duration_index')['duration'].agg(min).reset_index().values)
+
+def subj_label(df, default='ALL'):
+    subjs = df['subj'].unique()
+    return subjs[0].upper() if len(subjs) == 1 else default
+
+def plot_inner(ax, df):
+    durmap = dict(df.groupby('duration_index')['duration'].agg(min).reset_index().values)
+    cohs = sorted(df['coherence'].unique())
+    colmap = dict((coh, col) for coh, col in zip([0]*2 + cohs, color_list(len(cohs) + 2, "YlGnBu")))
+    for coh, df_coh in df.groupby('coherence'):
+        isp, ysp = zip(*df_coh.groupby('duration_index').agg(np.mean)['correct'].reset_index().values)
+        xsp = [durmap[i]*1000 for i in isp]
+        ax.plot(xsp, ysp, color=colmap[coh], label="%0.2f" % coh, marker='o', linestyle='-')
+
+def plot_info(ax, title):
+    plt.title('{0}: % correct vs. duration'.format(title))
+    plt.xlabel('duration')
+    plt.ylabel('% correct')
+    # plt.xscale('log')
+    # plt.xlim([30, 1050])
+    plt.ylim([0.4, 1.05])
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+def plot(args, isLongDur=False):
+    df = load(args, None, isLongDur)
+    subj = subj_label(df, '')
+    for dotmode, df_dotmode in df.groupby('dotmode'):
+        fig = plt.figure()
+        ax = plt.subplot(111)
+        title = '{0}, {1}'.format(dotmode, subj)
+        plot_inner(ax, df)
+        plot_info(ax, title)
+        plt.show()
+
+def write_json((xsp, ysp), (xs, ys, th), subj, dotmode, outdir):
+    outfile = os.path.join(outdir, 'pcorVsDurByCoh-{subj}-{dotmode}.json'.format(subj=subj, dotmode=dotmode))
+    with open(outfile, 'w') as f:
+        obj = {'binned': {'xs': list(xsp), 'ys': list(ysp)}, 'fit': {'xs': list(xs), 'ys': list(ys), 'theta': list(th)}}
+        json.dump(obj, f)
+
 def fit_curve(df, A=None, T=None):
     B = DEFAULT_THETA['sat-exp']['B']
     if A is None and T is None:
@@ -24,90 +67,29 @@ def fit_curve(df, A=None, T=None):
     sec_to_ms = lambda xs: [x*1000 for x in xs]
     return sec_to_ms(xs), ys, (A, B, T)
 
-def plot_inner(ax, df):
-    durmap = dict(df.groupby('duration_index')['duration'].agg(min).reset_index().values)
-    cohs = sorted(df['coherence'].unique())
-    colmap = dict((coh, col) for coh, col in zip([0]*2 + cohs, color_list(len(cohs) + 2, "YlGnBu")))
-    for coh, df_coh in df.groupby('coherence'):
-        isp, ysp = zip(*df_coh.groupby('duration_index').agg(np.mean)['correct'].reset_index().values)
-        xsp = [durmap[i]*1000 for i in isp]
-        ax.plot(xsp, ysp, color=colmap[coh], label="%0.2f" % coh, marker='o', linestyle='-')
-
-DEFAULT_FITS = {'ALL': {'2d': [0.84859761, 79.69870077], '3d': [0.74840758, 144.49747108]}}
-def plot_inner_across_cohs(ax, df, dotmode, savefig, outdir, fit_default=DEFAULT_FITS['ALL']):
-    color='g' if dotmode == '2d' else 'r'
-    durmap = dict(df.groupby('duration_index')['duration'].agg(min).reset_index().values)
-    isp, ysp = zip(*df.groupby('duration_index').agg(np.mean)['correct'].reset_index().values)
-    xsp = [durmap[i]*1000 for i in isp]
-    ax.plot(xsp, ysp, color=color, label=dotmode, marker='o', linestyle='')
-    # A, T = fit_default[dotmode]
-    A, T = None, None
-    xs, ys, th = fit_curve(df, A, T)
-    th = list(th)
-    th[-1] += 30 # for delay
-    if xs is not None:
-        ax.plot(xs, ys, color=color)
-        plt.axvline(th[-1], color=color, linestyle='--')
-        plt.text(th[-1] + 5, 0.5 if dotmode == '2d' else 0.45, 'tau={0:.2f}'.format(th[-1]), color=color)
-        plt.text(max(xsp) - 120, th[0] - 0.03, 'A={0:.2f}'.format(th[0]), color=color)
-        # plt.axhline(th[0], color=color, linestyle='dotted')
-    if savefig:
-        subjs = df['subj'].unique()
-        subj_label = subjs[0].upper() if len(subjs) == 1 else 'ALL'
-        outfile = os.path.join(outdir, 'pcorVsDurByCoh-{subj}-{dotmode}.json'.format(subj=subj_label, dotmode=dotmode))
-        with open(outfile, 'w') as f:
-            obj = {'binned': {'xs': list(xsp), 'ys': list(ysp)}, 'fit': {'xs': list(xs), 'ys': list(ys), 'theta': list(th)}}
-            json.dump(obj, f)
-
-def plot_info(ax, title):
-    plt.title('{0}: % correct vs. duration'.format(title))
-    plt.xlabel('duration')
-    plt.ylabel('% correct')
-    # plt.xscale('log')
-    # plt.xlim([30, 1050])
-    plt.ylim([0.4, 1.05])
-    box = ax.get_position()
-    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-
-def plot(ax, df, dotmode, show_for_each_dotmode, savefig, outdir):
-    if show_for_each_dotmode:
-        subjs = df['subj'].unique()
-        plot_inner(ax, df)
-        plot_info(ax, dotmode + ', {0}'.format(subjs[0].upper() if len(subjs) == 1 else ''))
-    else:
-        plot_inner_across_cohs(ax, df, dotmode, savefig, outdir)
-
-def main(args, show_for_each_dotmode, savefig=False, outdir=None, isLongDur=False):
+def fit(args, outdir, isLongDur=False):
     df = load(args, None, isLongDur)
-    subjs = df['subj'].unique()
-    if not show_for_each_dotmode:
-        fig = plt.figure()
-        ax = plt.subplot(111)
+    subj = subj_label(df)
+    durmap = durmap_fcn(df)
     for dotmode, df_dotmode in df.groupby('dotmode'):
-        if show_for_each_dotmode:
-            fig = plt.figure()
-            ax = plt.subplot(111)
-        plot(ax, df_dotmode, dotmode, show_for_each_dotmode, savefig, outdir)
-        if show_for_each_dotmode:
-            if savefig:
-                png_outfile = os.path.join(outdir, 'pcorVsDurByCoh-{subj}-{dotmode}.png')
-                plt.savefig(png_outfile.format(subj=subjs[0] if len(subjs) == 1 else 'ALL', dotmode=dotmode))
-            else:
-                plt.show()
-    if not show_for_each_dotmode:
-        plot_info(ax, 'all cohs' + ', {0}'.format(subjs[0].upper() if len(subjs) == 1 else ''))
-        if not savefig:
-            plt.show()
+        isp, ysp = zip(*df_dotmode.groupby('duration_index').agg(np.mean)['correct'].reset_index().values)
+        xsp = [1000*durmap[i] for i in isp]
+        xs, ys, th = fit_curve(df_dotmode)
+        th = list(th)
+        th[-1] += 30 # for delay
+        if outdir:
+            write_json((xsp, ysp), (xs, ys, th), subj, dotmode, outdir)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--subj", required=False, type=str, help="")
     parser.add_argument("-d", "--dotmode", required=False, type=str, help="")
-    parser.add_argument('--join-dotmode', action='store_true', default=False)
-    parser.add_argument('--savefig', action='store_true', default=False)
+    parser.add_argument('--fit', action='store_true', default=False)
     parser.add_argument('--outdir', type=str, default='.')
     parser.add_argument('--is-long-dur', action='store_true', default=False)
     args = parser.parse_args()
     ps = {'subj': args.subj, 'dotmode': args.dotmode}
-    main(ps, not args.join_dotmode, args.savefig, args.outdir, args.is_long_dur)
+    if args.fit:
+        fit(ps, args.outdir, args.is_long_dur)
+    else:
+        plot(ps, args.is_long_dur)
