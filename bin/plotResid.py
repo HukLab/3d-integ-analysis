942d0df9161ff80b491277b140c9b0324301d624
mobeets <mobeets@gmail.com>
2014-10-17 14:19:46 -0500

    residual plot updated

--- bin/plotResid.py
+++ bin/plotResid.py
@@ -23,7 +23,7 @@ rcParams['font.size'] = 14
 rcParams['axes.unicode_minus'] = False
 matplotlib.rc('lines', linewidth=2)
 
-def plot(df0a, df1a, df2a, dotmode, colorB):
+def plot(df0a, df1a, df2a, dotmode, colorA, colorB):
     df0 = df0a[df0a['dotmode'] == dotmode]
     df1 = df1a[df1a['dotmode'] == dotmode]
     df2 = df2a[df2a['dotmode'] == dotmode]
@@ -42,21 +42,21 @@ def plot(df0a, df1a, df2a, dotmode, colorB):
     ptB = df2[['b0','m0']].median()
     df['y2'] = df['dur']**ptB['m0']*np.exp(ptB['b0'])
 
-    colorA = '0.7'
-    sz = 50
-    lw = 2
+    sz = 80
+    lw1 = 1
+    lw2 = 2
     # colorB = [0.0, 0.7, 0.0]
     df['y1e'] = -(df['y1'] - df['mean'])/df['std']
     df['y2e'] = -(df['y2'] - df['mean'])/df['std']
-    plt.plot(df['dur'], df['y2e'], lw=lw, c='k', zorder=4)
-    plt.plot(df['dur'], df['y1e'], lw=lw, c=colorA, zorder=4)
-    plt.gca().fill_between(df['dur'], 0.0, df['y2e'], facecolor=colorB, alpha=1.0, zorder=3)
-    plt.gca().fill_between(df['dur'], 0.0, df['y1e'], facecolor=colorA, alpha=0.6, zorder=3)
-    plt.scatter(df['dur'], df['y2e'], sz, c=colorB, label='1-elb', zorder=5)
-    plt.scatter(df['dur'], df['y1e'], sz, c=colorA, label='2-elb', zorder=5)
+    # plt.plot(df['dur'], df['y2e'], lw=lw1, c='k', zorder=4)
+    # plt.plot(df['dur'], df['y1e'], lw=lw1, c='k', zorder=4)
+    plt.gca().fill_between(df['dur'], 0.0, df['y2e'], lw=0, facecolor=colorB, alpha=1.0, zorder=3)
+    plt.gca().fill_between(df['dur'], 0.0, df['y1e'], lw=0, facecolor=colorA, alpha=0.6, zorder=3)
+    plt.scatter(df['dur'], df['y2e'], sz, c=colorB, lw=lw1, label='bi-limb fit', zorder=5)
+    plt.scatter(df['dur'], df['y1e'], sz, c=colorA, lw=lw1, label='tri-limb fit', zorder=5)
 
-    plt.plot([np.exp(ptA['x0']), np.exp(ptA['x0'])], [-30, 30], '--', lw=lw, c=colorA, zorder=1)
-    plt.plot([0.01, df['dur'].max() + 50], [0, 0], '-', lw=lw, c='k', zorder=1)
+    plt.plot([np.exp(ptA['x0']), np.exp(ptA['x0'])], [-30, 30], '--', lw=lw2, c=colorA, zorder=1)
+    plt.plot([0.01, df['dur'].max() + 50], [0, 0], '-', lw=lw2, c='k', zorder=1)
 
     # plt.plot(df['dur'], df['mean'])
     # plt.scatter(df['dur'], df['y1'], c='g')
@@ -66,8 +66,8 @@ def plot(df0a, df1a, df2a, dotmode, colorB):
     plt.xscale('log')
     # plt.xlim(df0a['dur'].min()-2, df['dur'].max()+30)
     # plt.ylim(ys.min()-1, ys.max()+1)
-    plt.xlabel('duration (msec)')
-    plt.ylabel('motion sensitivity residual')
+    plt.xlabel('Duration (msec)')
+    plt.ylabel('Motion sensitivity residual')
     leg = plt.legend(loc='upper left', prop={'size': 14})
     leg.get_frame().set_linewidth(1.5)
     xtcks = np.array([50, 100, 500])
@@ -82,25 +82,25 @@ def plot(df0a, df1a, df2a, dotmode, colorB):
     plt.gca().tick_params(axis='y', direction='out', width=1.5, length=5)
     plt.gcf().patch.set_facecolor('white')
     for axis in ['bottom', 'left']:
-        plt.gca().spines[axis].set_linewidth(lw)
+        plt.gca().spines[axis].set_linewidth(lw2)
     for axis in ['top', 'right']:
         plt.gca().spines[axis].set_linewidth(0)
 
 def main():
-    yrng = (-17.0, 8.0)
+    yrng = (-17.0, 10.0)
     xrng = (31.0, 1013.0)
     df0, df1, df2 = load()
-    plot(df0, df1, df2, '2d', [0.0, 0.7, 0.0])
+    plot(df0, df1, df2, '2d', colorA=[0.6, 0.6, 0.7], colorB=[0.2, 0.4, 1.0])
     plt.xlim(xrng)
     plt.ylim(yrng)
     # plt.show()
-    plt.savefig('../plots/resid-2d.png')
+    plt.savefig('../plots/elbowResiduals-ALL-2d.png')
     plt.clf()
-    plot(df0, df1, df2, '3d', [0.7, 0.0, 0.0])
+    plot(df0, df1, df2, '3d', colorA=[0.7, 0.6, 0.6], colorB=[0.9, 0.1, 0.1])
     plt.xlim(xrng)
     plt.ylim(yrng)
     # plt.show()
-    plt.savefig('../plots/resid-3d.png')
+    plt.savefig('../plots/elbowResiduals-ALL-3d.png')
 
 if __name__ == '__main__':
     main()
